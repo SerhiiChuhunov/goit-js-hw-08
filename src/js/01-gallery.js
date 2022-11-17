44ff168f3bb93128eb7680c2774832224cf26396
Serhii Chuhunov <patricknamaste@gmail.com>
2022-11-17 18:23:17 +0200

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,5 @@
 // Add imports above this line
-import galleryItems from './gallery-items.js';
+import galleryItems from './gallery-items.json';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
